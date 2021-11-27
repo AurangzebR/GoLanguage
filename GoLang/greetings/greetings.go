bbb34c080514bb00126d9b1c3eccafea8451206c
Aurangzeb Riasat(Zeb) <azriasat@gmail.com>
2021-11-26 19:06:33 -0500

    used math/rand and time to print random statements

--- GoLang/greetings/greetings.go
+++ GoLang/greetings/greetings.go
@@ -3,13 +3,26 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func Hello(name string) (string, error) {
 	if name == "" {
-		return "", errors.New("empty name")
+		return "name", errors.New("empty name")
 	}
-	message := fmt.Sprintf("Hello %v, welcome", name)
+	message := fmt.Sprintf(randomFormat(), name)
 
 	return message, nil
+}
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+func randomFormat() string {
+	formats := []string{
+		"hi, %v. welcome",
+		"Great to see you,%v",
+		"Hail, %v! well met!"}
+	return formats[rand.Intn(len(formats))]
+
 }
